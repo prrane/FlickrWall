98385a4cffbe7ff31d621f7045258969e1340e9c
Prashant Rane <prrane@microsoft.com>
2018-06-29 18:59:10 +0530

    Initialize project

--- FlickrWall/MainViewController.swift
+++ FlickrWall/MainViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    title = NSLocalizedString("Flickr Feed", comment: "Main view title for flickr app")
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
 
 }
 
